b11cd94debbd54d729cdef00c9a8acf0bec2b6cd
Timur <cube398@gmail.com>
2021-05-23 12:57:16 +0300

    commi2

--- src/App.js
+++ src/App.js
@@ -1,39 +1,36 @@
 import React from "react";
 import "./App.css";
 import Block from "./Block";
-class Data extends React.Component {
+
+
+class App extends React.Component {
+  
   state = {
     isHide: false,
   };
 
-  componentDidMount() {
-    alert("1")
+  componentDidMount()
+  {
+    alert('1')
   }
 
-  componentWillUnmount() {
-    alert("1")
+  componentWillUnmount() 
+  {
+    alert('2')
   }
+
 
   render() {
-    console.log("render from return");
     return (
-      <div className = "App">
-        <button onClick={() => this.setState({ isHide: !this.state.isHide })}>
+      <div className="App">
+      <button onClick={() => this.setState({ isHide: !this.state.isHide })}>
         Click Me!
-        </button>
-        {this.state.isHide && <Block />}
-      </div>
+      </button>
+      {this.state.isHide && <Block />}
+    </div>
+
     );
   }
 }
-
-const App = () => {
-  return (
-    <div className="App">
-      {/* <Block /> */}
-      <Data />
-    </div>
-  );
-};
 
 export default App;
--- src/Block.js
+++ src/Block.js
@@ -1,17 +1,13 @@
 import React from "react";
 
-
 class Block extends React.Component {
-    render() {
-        return (
-            <div >
-                <h1>Hello world!!!</h1>
-            </div>
-
-        )
-
-    }
-
+  render() {
+    return (
+      <div>
+        <h1>Hello world!!!</h1>
+      </div>
+    );
+  }
 }
 
 export default Block;
